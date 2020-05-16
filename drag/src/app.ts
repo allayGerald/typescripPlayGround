768c9c0277ac26159063ae15c4199104ad9e5a82
Gerald Simon <gerald@adroittech.co.tz>
2020-05-16 16:47:09 +0300

    feat autoBind decorator

--- drag/src/app.ts
+++ drag/src/app.ts
@@ -1,7 +1,27 @@
+/**
+ * autoBind decorator
+ * */
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get(): any {
+      return originalMethod.bind(this);
+    }
+  };
+
+  return adjDescriptor;
+}
+
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+
+  titleElement: HTMLInputElement;
+  descriptionElement: HTMLInputElement;
+  peopleElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,11 +30,27 @@ class ProjectInput {
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = "user-input";
+
+    this.titleElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleElement = this.element.querySelector('#people') as HTMLInputElement;
+    this.configure();
+
     this.attach();
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private configure(){
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  @autoBind
+  private submitHandler(event: Event){
+    event.preventDefault();
+    console.log(this.titleElement.value);
   }
 }
 
